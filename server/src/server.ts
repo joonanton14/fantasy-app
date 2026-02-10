177233814a72f07350fde8752f63dc46dcb91b21
Joona Sollo <128515340+joonanton14@users.noreply.github.com>
2026-02-10 15:09:15 +0200

    cors fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,6 +16,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
 
 // Prefix all API routes with /api
