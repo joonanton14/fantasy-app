3d3c7711ff51b3c5fdb1e54884ebdbae6b4b3483
Joona Sollo <128515340+joonanton14@users.noreply.github.com>
2026-02-12 13:31:31 +0200

    final fix for fixes

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,11 @@
 import { Redis } from "@upstash/redis";
 
-export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.KV_REST_API_URL!,
-});
+const url = process.env.KV_REST_API_URL;
+const token = process.env.KV_REST_API_TOKEN;
 
+if (!url || !token) {
+  throw new Error("Missing KV_REST_API_URL or KV_REST_API_TOKEN");
+}
+
+export const redis = new Redis({ url, token });
 export const PREFIX = "fantasy";
