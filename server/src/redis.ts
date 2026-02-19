cf8fd9b9db62257d22b30206def3ae2459f66c32
Joona Sollo <128515340+joonanton14@users.noreply.github.com>
2026-02-19 11:16:54 +0200

    maybe now

--- server/src/redis.ts
+++
@@ -1,8 +0,0 @@
-import Redis from 'ioredis';
-
-const redisUrl = process.env.REDIS_URL;
-if (!redisUrl) {
-  console.warn('REDIS_URL is not set. Sessions will not work correctly in production.');
-}
-
-export const redis = redisUrl ? new Redis(redisUrl) : null;
